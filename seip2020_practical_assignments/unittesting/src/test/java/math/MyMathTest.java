31e5c6378aff1b8b44b95744308d84d3367926af
IreneArapogiorgi <irenearapogiorgi@gmail.com>
2020-04-16 14:13:23 +0300

    Update MyMathTest.java

--- seip2020_practical_assignments/unittesting/src/test/java/math/MyMathTest.java
+++ seip2020_practical_assignments/unittesting/src/test/java/math/MyMathTest.java
@@ -1,6 +1,7 @@
 package math;
 
 import org.junit.Test;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
@@ -16,13 +17,31 @@ public class MyMathTest {
 	
 	MyMath mm = new MyMath();
 	
+	/*
+	 * A unit test that checks correctness of isPrime
+	 * method when input value n is a prime number.
+	 */
+    @Test
+    public void testIsPrimeTrueOutput() {
+    	Assert.assertEquals(true, mm.isPrime(5));
+    }
+    
+	/*
+	 * A unit test that checks correctness of isPrime
+	 * method when input value n is a non-prime number.
+	 */
+    @Test
+    public void testIsPrimeFalseOutput() {
+    	Assert.assertEquals(false, mm.isPrime(4));
+    }
+	
     @Rule
 	public ExpectedException thrown = ExpectedException.none(); // Initialize it to .none()
     
     /*
 	 * A test case for the exception caused when
-	 * parameter n is negative. Testing the
-	 * exception is performed with a @Rule
+	 * parameter n is negative in factorial method.
+	 * Testing the exception is performed with a @Rule
 	 */
 	@Test 
 	public void testFactorialShouldThrowExceptionOnNegativeInput() {
@@ -33,13 +52,25 @@ public class MyMathTest {
 	
 	/*
 	 * A test case for the exception caused when
-	 * parameter n is larger than 20. Testing
-	 * the exception is performed with a @Rule
+	 * parameter n is larger than 20 in factorial method.
+	 * Testing the exception is performed with a @Rule
 	 */
 	@Test
 	public void testFactorialShouldThrowExceptionOnBigInput() {
 		thrown.expect(IllegalArgumentException.class);
 		thrown.expectMessage("Parameter n cannot be larger than 20.");
 		mm.factorial(25);
+	}
+	
+	/*
+	 * A test case for the exception caused when
+	 * parameter n is smaller than 2 in isPrime method.
+	 * Testing the exception is performed with a @Rule
+	 */
+	@Test
+	public void testIsPrimeShouldThrowExceptionOnInputSmallerThanTwo() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Parameter n cannot be smaller than 2.");
+		mm.isPrime(1);
 	}
 }
