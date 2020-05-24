99ce35507129083549106f63b64f3757e4b2f001
IreneArapogiorgi <irenearapogiorgi@gmail.com>
2020-05-24 17:15:50 +0300

    Re-format StrcompAnalyzer.java

--- seip2020_practical_assignments/codeanalyzer/src/main/java/metricsmanager/StrcompAnalyzer.java
+++ seip2020_practical_assignments/codeanalyzer/src/main/java/metricsmanager/StrcompAnalyzer.java
@@ -23,8 +23,9 @@ public class StrcompAnalyzer extends AnalyzerType {
 		int nonCodeLinesCounter = 0;
 		for (String line : sourceCodeList) {
 			line = line.trim(); //clear all leading and trailing white spaces
-			if (line.startsWith("//") || line.startsWith("/*") || line.startsWith("*") || line.equals("{") || line.equals("}") || line.equals(""))
+			if (line.startsWith("//") || line.startsWith("/*") || line.startsWith("*") || line.equals("{") || line.equals("}") || line.equals("")) {
 				nonCodeLinesCounter++;
+			}
 		}
 		int loc = sourceCodeList.size() - nonCodeLinesCounter;
 		return loc;
@@ -36,10 +37,9 @@ public class StrcompAnalyzer extends AnalyzerType {
 		int methodCounter = 0;
 		for (String line : sourceCodeList) {
 			line = line.trim(); //clear all leading and trailing white spaces
-			if ( ((line.contains("public") || line.contains("private") || line.contains("protected"))
-					|| line.contains("void") || line.contains("int") || line.contains("String"))
-				&& line.contains("(") && line.contains(")") && line.contains("{") )
+			if ( ((line.contains("public") || line.contains("private") || line.contains("protected")) || line.contains("void") || line.contains("int") || line.contains("String")) && line.contains("(") && line.contains(")") && line.contains("{") ) {
 				methodCounter++;
+			}
 		}
 		return methodCounter;
 	}
